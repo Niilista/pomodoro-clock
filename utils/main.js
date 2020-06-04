337010084cd27372fffbe50ac5de5a312e540f31
Victor Hugo <victorhugo.vhs@hotmail.com>
2020-06-04 15:44:35 -0300

    Clock fixed, added some style

--- utils/main.js
+++ utils/main.js
@@ -1,24 +1,29 @@
-var clock = document.getElementById("clock");
 var secInput = document.getElementById("second");
 var minInput = document.getElementById("minute");
 var hourInput = document.getElementById("hour");
 var start = document.getElementById("start");
+var pause = document.getElementById("pause");
+var stopClock = document.getElementById("stop");
+var flag = false;
+var keepGoing = true;
 
-Notification.requestPermission();
+//Notification.requestPermission();
 
-function spawnNotification(corpo, icone, titulo) {
-    var opcoes = {
-        body: corpo,
-        icon: icone
-    }
-    var n = new Notification(titulo, opcoes);
-}
-
+secInput.addEventListener("input", function () {
+    secInput.value = checkTime(secInput.value);
+});
+minInput.addEventListener("input", function () {
+    minInput.value = checkTime(minInput.value);
+});
+hourInput.addEventListener("input", function () {
+    hourInput.value = checkTime(hourInput.value);
+});
 
 start.addEventListener("click", function () {
     var hours = document.getElementById("hour").value;
     var minutes = document.getElementById("minute").value;
     var seconds = document.getElementById("second").value;
+    keepGoing = true;
     if (!hours) {
         hours = 0;
     }
@@ -31,14 +36,39 @@ start.addEventListener("click", function () {
     getClock(hours, minutes, seconds);
 });
 
+pause.addEventListener("click", function () {
+    keepGoing = false;
+});
+
+stopClock.addEventListener("click", function () {
+    console.log("stop");
+});
+
+
+function spawnNotification(corpo, icone, titulo) {
+    var opcoes = {
+        body: corpo,
+        icon: icone
+    }
+    var n = new Notification(titulo, opcoes);
+}
+
 function getClock(hours, minutes, seconds) {
 
-    console.log(hours + ':' + minutes + ':' + seconds);
-    secInput.value = seconds;
-    minInput.value = minutes;
-    hourInput.value = hours;
+    var showHour = checkTime(hours);
+    var showMin = checkTime(minutes);
+    var showSec = checkTime(seconds);
 
-    var flag = false;
+    if( keepGoing == false ){
+        clearTimeout();
+        return;
+    }
+
+    secInput.value = showSec;
+    minInput.value = showMin;
+    hourInput.value = showHour;
+
+    flag = false;
 
     if (seconds == 0) {
         if (minutes == 0) {
@@ -59,10 +89,17 @@ function getClock(hours, minutes, seconds) {
 
 
     if (flag == true) {
-        console.log("notificação");
         new Notification("Relógio Finalizado!");
         return;
-    } else {
+    }else {
         setTimeout(function () { getClock(hours, minutes, seconds); }, 1000);
     }
+}
+
+function checkTime(num) {
+    var string = num + "";
+    if (num < 10 && string.length < 2) {
+        string = "0" + string;
+    }
+    return string;
 }
